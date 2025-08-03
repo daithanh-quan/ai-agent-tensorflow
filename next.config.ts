e2eb2986174916615cc7e6fcfe9b45c92ee5f009
thanh <daithanhdn97@gmail.com>
2025-08-03 21:32:40 +0700

    add config for webpack

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,5 @@
+import path from "path";
+
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
@@ -5,6 +7,18 @@ const nextConfig: NextConfig = {
   env: {
     API_URL: process.env.NEXT_PUBLIC_API_URL,
     SESSION_PASSWORD: process.env.SESSION_PASSWORD,
+  },
+  webpack: (config) => {
+    // Add path aliases to webpack
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./src"),
+      src: path.resolve(__dirname, "./src"),
+    };
+    return config;
+  },
+  typescript: {
+    ignoreBuildErrors: false,
   },
 };
 
